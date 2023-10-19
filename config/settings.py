5266f96ed778c9442740e22e5c1e298342d8f972
Bekdeveloper77 <139876342+Bekdeveloper77@users.noreply.github.com>
2023-10-19 10:18:37 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -169,7 +169,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/django-static/'
+STATIC_URL = 'django-static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'django-static/')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
